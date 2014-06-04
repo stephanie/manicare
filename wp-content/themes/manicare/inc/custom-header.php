dad3ba56a2096a9f3397f88aad99d08a7897b0d1
Stephanie Siaw <siaw.stephanie@gmail.com>
2014-06-05 00:35:24 +0800

    fixed background image

--- wp-content/themes/manicare/inc/custom-header.php
+++ wp-content/themes/manicare/inc/custom-header.php
@@ -19,11 +19,11 @@
  * @package landscape
  */
 $args = array(
-	'width'         => 1440,
-	'height'        => 500,
-	'default-image' => get_template_directory_uri() . '/images/default-header.jpg',
-	'header-text'   => false,
-	'uploads'       => true,
+  'width'         => 1440,
+  'height'        => 500,
+  'default-image' => get_template_directory_uri() . '/images/default-header.jpg',
+  'header-text'   => false,
+  'uploads'       => true,
 );
 add_theme_support( 'custom-header', $args );
 
@@ -38,16 +38,16 @@ if ( ! function_exists( 'landscape_admin_header_image' ) ) :
  * @since landscape 1.0
  */
 function landscape_admin_header_image() { ?>
-	<div id="headimg">
-		<?php
-		if ( 'blank' == get_header_textcolor() || '' == get_header_textcolor() )
-			$style = ' style="display:none;"';
-		else
-			$style = ' style="color:#' . get_header_textcolor() . ';"';
-		?>
-		<h1><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<div id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
-	</div>
+  <div id="headimg">
+    <?php
+    if ( 'blank' == get_header_textcolor() || '' == get_header_textcolor() )
+      $style = ' style="display:none;"';
+    else
+      $style = ' style="color:#' . get_header_textcolor() . ';"';
+    ?>
+    <h1><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+    <div id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
+  </div>
 <?php }
 endif; // landscape_admin_header_image
 
@@ -60,54 +60,10 @@ if ( ! function_exists( 'landscape_custom_header_image' ) ) :
  */
 function landscape_custom_header_image() {
 
-	$header_image = get_header_image();
-	global $content_width;
+  $header_image = get_header_image();
+  global $content_width;
 ?>
 
-	<style type="text/css">
-
-	<?php
-
-	if ( ! empty( $header_image ) ) :
-
-		$background = "rgba(0,0,0,.7)"; ?>
-
-		#masthead {
-			background: #111 url( <?php echo esc_url( $header_image ); ?> ) center 0 no-repeat;
-			margin-top: 0;
-			padding-bottom: 0;
-			max-width: 100%;
-			height: <?php echo get_custom_header()->height; ?>px;
-		}
-
-	<?php endif; // !empty header_image()
-
-	if ( is_single() || is_page() ) :
-
-		if ( '' != get_the_post_thumbnail() ) :
-
-			$featured_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'featured-thumbnail' );
-			$header_image = "background: #111 url(" . $featured_image_url[0] . ") center 0 no-repeat;";
-			$menubackground = "rgba(0,0,0,.7)";
-			$mastheadbackground = "#111";
-			$mastheadheight = "500px";
-			$paddingbottom = "0"; ?>
-
-
-			#masthead {
-				<?php echo $header_image; ?>
-				margin-top: 0;
-				padding-bottom: <?php echo $paddingbottom; ?>;
-				max-width: 100%;theadheight; ?>;
-				position: relative;
-				background-color: <?php echo $mastheadbackground; ?>;
-			}
-			#masthead hgroup{
-				display:none;
-			}
-		<?php endif; //'' != get_the_post_thumbnail() ?>
-	<?php endif; //is_single() && ! is_attachment() ?>
-	</style>
 <?php
 }
 endif; // landscape_custom_header_image
